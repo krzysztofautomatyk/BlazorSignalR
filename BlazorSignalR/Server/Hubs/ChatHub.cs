6760295addbb219bdd4dc9377ff7eb2458741511
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2022-02-21 20:43:43 +0100

    Finish the Chat with SignalR

--- BlazorSignalR/Server/Hubs/ChatHub.cs
+++ BlazorSignalR/Server/Hubs/ChatHub.cs
@@ -4,12 +4,22 @@ namespace BlazorSignalR.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
         public override async Task OnConnectedAsync()
         {
-            await AddMessageToChat("", "User connected!");
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username); // key / value
+            await AddMessageToChat(string.Empty, $"{username} is connected!");
             await base.OnConnectedAsync();
         }
 
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} is left!");
+        }
         public async Task AddMessageToChat(string user, string message)
         { 
             await Clients.All.SendAsync("GetThatMessage",user, message);
