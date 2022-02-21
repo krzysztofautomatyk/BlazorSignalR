61ca9cf208c1ec0468c817db7d1a4735cbac957b
Krzysztof Woźniczak <krzysztofautomatyk@gmail.com>
2022-02-21 20:23:00 +0100

    Add project files.

---
+++ BlazorSignalR/Server/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalR.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("", "User connected!");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task AddMessageToChat(string user, string message)
+        { 
+            await Clients.All.SendAsync("GetThatMessage",user, message);
+        }
+
+    }
+}
